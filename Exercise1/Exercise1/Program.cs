af6e7e1c6e4f71816b518435152cf92e49e63259
Alyssa Williams <alyssaw90@gmail.com>
2016-07-19 11:20:46 -0700

    added OOPLab exercise 1

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -10,6 +10,19 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("**Let's guess the square root of numbers**");
+            Console.WriteLine("Please enter a positive number: ");
+            string userNumberInput = Console.ReadLine();
+            double number = double.Parse(userNumberInput);
+            Console.WriteLine("Please enter an error limit: ");
+            string userErrorLimitInput = Console.ReadLine();
+            double errorlimit = double.Parse(userErrorLimitInput);
+
+            SquareRoot mySqrt = new SquareRoot(errorlimit);
+            double result = mySqrt.ComputeSquareRoot(number);
+
+            Console.WriteLine("The result is {0}", result);
+            Console.ReadLine();
         }
     }
 }
--- Exercise1/Exercise1/SquareRoot.cs
+++ Exercise1/Exercise1/SquareRoot.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Exercise1
 {
-    class SquareRoot
+    public class SquareRoot
     {
+        //Constructor accepts error limit *1 as a double
+        public SquareRoot(double limit)
+        {
+            this.errorlimit = limit;
+        }
+        public double errorlimit { get; set; }
+
+        //Method to guess the square root of a number.
+        //Takes in the number and an error limit. 
+        public double ComputeSquareRoot(double number)
+        {
+            //Checks to make sure number is positive
+            if (number < 1)
+            {
+                throw new System.ArgumentException("Negative numbers are not allowed.");
+            }
+
+            //Initial guess starts as the number divided by 2
+            double guess = number / 2;
+
+            //If the differece is greater than the error limit then need to guess again by
+            //taking the average of the first guess and the number divided by the previous guess
+            while (Math.Abs(number - (guess * guess)) > errorlimit)
+            {
+                guess = (guess + (number / guess)) * .5;
+            }
+
+            return guess;
+        }
     }
 }
